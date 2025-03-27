f0f4f11bc32aa195e62cff4a91ecf786fffad6eb
DhanashriAGhotkar <ghotkardhanashri1006@gmail.com>
2025-03-27 13:13:50 -0400

    3 rs commit by dhanashri from vscode

--- GitPracticeByDhanashri2025/src/Demo1.java
+++ GitPracticeByDhanashri2025/src/Demo1.java
@@ -8,6 +8,10 @@ public class Demo1 {
 	public void m2(){
 		System.out.println("Hello from m2..Github");
 	}
+
+	public void m3(){
+		System.out.println("Hello..from m3..VsCode");
+	}
 	public static void main(String[] args) {
 		System.out.println("This is Git Demo1..!");
 	}
