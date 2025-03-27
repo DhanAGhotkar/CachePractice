4f0f595bb8075567544826aea6048f9585512693
DhanashriAGhotkar <ghotkardhanashri1006@gmail.com>
2025-03-27 12:25:52 -0400

    2nd commit by Dhanashri. added m1 method

--- GitPracticeByDhanashri2025/src/Demo1.java
+++ GitPracticeByDhanashri2025/src/Demo1.java
@@ -1,5 +1,9 @@
 
 public class Demo1 {
+	
+	public void m1() {
+		System.out.println("Hello from m1");
+	}
 	public static void main(String[] args) {
 		System.out.println("This is Git Demo1..!");
 	}
