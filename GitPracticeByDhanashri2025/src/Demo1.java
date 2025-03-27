4b9ad38483974bc9ebf3df91d197d2c7fe58e32a
DhanAGhotkar <ghotkardhanashri1006@gmail.com>
2025-03-27 12:29:04 -0400

    Update Demo1.java
    m2 method added in git by Dhanashri

--- GitPracticeByDhanashri2025/src/Demo1.java
+++ GitPracticeByDhanashri2025/src/Demo1.java
@@ -4,6 +4,10 @@ public class Demo1 {
 	public void m1() {
 		System.out.println("Hello from m1");
 	}
+
+	public void m2(){
+		System.out.println("Hello from m2..Github");
+	}
 	public static void main(String[] args) {
 		System.out.println("This is Git Demo1..!");
 	}
